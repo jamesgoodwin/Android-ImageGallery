48ae02fe5517c3561b9d58b32db000c4bd6e4b2c
jamesgoodwin <jamesgoodwin@gmail.com>
2017-05-16 07:45:51 +0100

    Format code

--- app/src/main/java/com/imagegallery/fullscreen/FullscreenImageActivity.kt
+++ app/src/main/java/com/imagegallery/fullscreen/FullscreenImageActivity.kt
@@ -33,7 +33,12 @@ class FullscreenImageActivity : AppCompatActivity() {
 
         imageView.setOnClickListener { this@FullscreenImageActivity.toggle() }
 
-        hidePart2Runnable = Runnable { imageView.systemUiVisibility = SYSTEM_UI_FLAG_LOW_PROFILE or SYSTEM_UI_FLAG_FULLSCREEN or SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_IMMERSIVE_STICKY or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or SYSTEM_UI_FLAG_HIDE_NAVIGATION }
+        hidePart2Runnable = Runnable { imageView.systemUiVisibility = SYSTEM_UI_FLAG_LOW_PROFILE or
+                SYSTEM_UI_FLAG_FULLSCREEN or
+                SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                SYSTEM_UI_FLAG_HIDE_NAVIGATION }
 
         showPart2Runnable = Runnable {
             supportActionBar?.show()
